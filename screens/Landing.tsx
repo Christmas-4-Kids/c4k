0255d62d2332444dd3327b5dec5d8f1eeb771bab
Conner Bush <cbush@evernote.com>
2021-10-08 09:19:29 -0500

    updates landing page with comments for direction

--- screens/Landing.tsx
+++ screens/Landing.tsx
@@ -1,18 +1,35 @@
-import React, { useEffect, useState } from "react"
-import { View, StatusBar, useColorScheme, Text } from "react-native"
+import React, { useState } from "react"
+import { View, StatusBar, useColorScheme, Text, TextInput, NativeSyntheticEvent, TextInputChangeEventData, Button } from "react-native"
 import styles from "../styles"
 import Navigation from "../navigation"
 
-const Landing = props => {
+export const Landing = () => {
   const colorScheme = useColorScheme()
   const [phoneNumber, setPhoneNumber] = useState("")
-  const [verificationCode, setVerificationCode] = useState()
-  const [twilioVerificationCode, setTwilioVerificationCode] = useState()
+  const [verificationCode, setVerificationCode] = useState("")
+  const [twilioVerificationCode, setTwilioVerificationCode] = useState("")
   const [userIsVerified, setUserIsVerified] = useState(false)
+  const [phoneNumberIsVerified, setPhoneNumberIsVerified] = useState(false)
 
-  useEffect(() => {
-    setUserIsVerified(!!twilioVerificationCode && verificationCode === twilioVerificationCode)
-  }, [twilioVerificationCode, verificationCode])
+  const phoneNumberIsRegistered = () => {
+    // TODO: send number to mailchimp to verify
+    // TODO: if number is valid then fetch twilioVerificationCode from Firebase using phoneNumber
+    //      -> set twilioVerificationCode
+    //      -> return true
+    // TODO: if number is not valid return false
+
+    // return true for now
+    return true
+  }
+  const verifyPhoneNumber = () => {
+    if (phoneNumberIsRegistered()) {
+      setPhoneNumberIsVerified(true)
+    }
+  }
+
+  const verifyUser = () => {
+    setUserIsVerified(twilioVerificationCode !== "" && verificationCode === twilioVerificationCode)
+  }
 
   return (
     <View style={styles.page}>
@@ -22,10 +39,29 @@ const Landing = props => {
           <StatusBar barStyle="light-content" />
         </View>
       ) : (
-        <Text style={styles.sectionText}>Code</Text>
+        <View style={styles.sectionContainer}>
+          <View style={styles.sectionContainer}>
+            {/* TODO: style text */}
+            <Text style={styles.sectionText}>Enter the phone number you used to register</Text>
+            <TextInput style={styles.textInput} onChange={(e: NativeSyntheticEvent<TextInputChangeEventData>) => setPhoneNumber(e.nativeEvent.text)} value={phoneNumber} />
+            {/* TODO: style button */}
+            <Button title="Verify Phone Number" onPress={verifyPhoneNumber} />
+          </View>
+          {phoneNumberIsVerified && (
+            <View style={styles.sectionContainer}>
+              {/* TODO: style text */}
+              <Text style={styles.sectionText}>Enter verification code</Text>
+              <TextInput
+                style={styles.textInput}
+                onChange={(e: NativeSyntheticEvent<TextInputChangeEventData>) => setVerificationCode(e.nativeEvent.text)}
+                value={verificationCode}
+              />
+              {/* TODO: style button*/}
+              <Button title="Submit" onPress={verifyUser} />
+            </View>
+          )}
+        </View>
       )}
     </View>
   )
 }
-
-export default Landing
