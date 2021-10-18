c9932f1b589494b763f7f68c0518ef12c5734357
Thomas Harris <loden16harris@gmail.com>
2021-10-18 17:40:52 -0500

    fix: add Rules tab

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,14 @@ const functions = require("firebase-functions")
 const admin = require("firebase-admin")
 const mailchimp = require("@mailchimp/mailchimp_marketing")
 const twilio = require("twilio")
+const Firestore = require("@google-cloud/firestore")
+const PROJECTID = "c4k-events"
+const COLLECTION_NAME = "rules"
+
+const firestore = new Firestore({
+  projectId: PROJECTID,
+  timestampsInSnapshots: true,
+})
 
 admin.initializeApp(functions.config().firebase)
 
@@ -25,7 +33,7 @@ exports.verifyNumber = functions.https.onCall(async (phoneNumber, context) => {
   twilioClient.verify
     .services(serviceSid)
     .verifications.create({ to: phoneNumber, channel: "sms" })
-    .then(verification => {
+    .then((verification) => {
       return verification
     })
 })
@@ -35,31 +43,49 @@ exports.verifyCode = functions.https.onCall(async (data, context) => {
   return twilioClient.verify
     .services(serviceSid)
     .verificationChecks.create({ to: data.phoneNumber, code: data.code })
-    .then(data => {
+    .then((data) => {
       return data.status
     })
 })
 
 exports.checkIfRegistered = functions.https.onCall(async (email, context) => {
   const response = await mailchimp.searchMembers.search(email)
-  const mailchimpMember = response?.exact_matches?.members.find(member => member.list_id === currentListId)
+  const mailchimpMember = response?.exact_matches?.members.find(
+    (member) => member.list_id === currentListId
+  )
   return mailchimpMember
 })
 
-exports.createMailchimpUserInFirestore = functions.https.onCall(async (mailchimpMember, context) => {
-  const volunteer = createVolunteer(mailchimpMember)
-  let documentRef = admin.firestore().doc("volunteers/" + mailchimpMember.id)
-  return documentRef
-    .set(volunteer)
-    .then(res => {
-      return { user: volunteer }
+exports.createMailchimpUserInFirestore = functions.https.onCall(
+  async (mailchimpMember, context) => {
+    const volunteer = createVolunteer(mailchimpMember)
+    let documentRef = admin.firestore().doc("volunteers/" + mailchimpMember.id)
+    return documentRef
+      .set(volunteer)
+      .then((res) => {
+        return { user: volunteer }
+      })
+      .catch((err) => {
+        return { error: `Failed to create document: ${err}` }
+      })
+  }
+)
+
+exports.fetchRules = functions.https.onCall(async () => {
+  let rules = []
+  await firestore
+    .collection(COLLECTION_NAME)
+    .get()
+    .then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        rules.push(doc.data())
+      })
     })
-    .catch(err => {
-      return { error: `Failed to create document: ${err}` }
-    })
+  return rules
 })
 
-const createVolunteer = mailchimpMember => {
+const createVolunteer = (mailchimpMember) => {
   const volunteer = {
     checkedIn: false,
     volunteerType: mailchimpMember.list_id,
