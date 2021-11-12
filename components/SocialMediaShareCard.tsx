179f3f739ef4ae523a70e8b7b792080216ca1fa5
Jason <10107412+jason1985@users.noreply.github.com>
2021-11-12 15:49:23 -0800

    add SocialMediaShareCard Component and gradient bg png

--- components/SocialMediaShareCard.tsx
+++ components/SocialMediaShareCard.tsx
@@ -1,15 +1,43 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from "react"
+import { View, ImageBackground } from "react-native"
 import { useStyles } from "../context/styles.context"
+import { FontAwesome5 } from "@expo/vector-icons"
+import gradientBg from "../assets/images/gradient.png"
 
 const SocialMediaShareCard = () => {
-	const { styles } = useStyles()
+  const { styles } = useStyles()
 
-	return (
-		<View style={styles.socialMediaShareCard}>
-			<Text>test</Text>
-		</View>
-	)
+  return (
+    <View style={styles.socialMediaShareCard}>
+      <ImageBackground
+        source={{
+          uri: gradientBg,
+        }}
+        style={{
+          alignSelf: "center",
+          padding: 0,
+          width: "100%",
+          height: "70px",
+          overflow: "hidden",
+          borderRadius: 10,
+          flexDirection: "row",
+        }}
+      >
+        <View style={styles.socialMediaShareCardLeftText}>
+          HELP US REACH MORE PEOPLE
+        </View>
+        <View style={styles.socialMediaShareCardRightText}>
+          Share C4K On Your Socials:
+          <View style={styles.socialMediaShareCardIcons}>
+            <FontAwesome5 name="facebook-square" size={14} color="#FFF" />
+            <FontAwesome5 name="instagram" size={14} color="#FFF" />
+            <FontAwesome5 name="twitter" size={14} color="#FFF" />
+            <FontAwesome5 name="linkedin-in" size={14} color="#FFF" />
+          </View>
+        </View>
+      </ImageBackground>
+    </View>
+  )
 }
 
 export default SocialMediaShareCard
