92556b4274bc23414f18dc4f631d9000ef3a512d
Conner Bush <cbush@evernote.com>
2021-11-15 12:51:49 -0600

    cleanup for iOS

--- components/SocialMediaShareCard.tsx
+++ components/SocialMediaShareCard.tsx
@@ -1,51 +1,54 @@
 import React from "react"
-import { View, Text, ImageBackground } from "react-native"
+import { View, Text, ImageBackground, Share } from "react-native"
 import { useStyles } from "../context/styles.context"
 import { FontAwesome5 } from "@expo/vector-icons"
+import { TouchableOpacity } from "react-native-gesture-handler"
 
 const SocialMediaShareCard = () => {
   const { styles } = useStyles()
+  const socialImageSize = 18
+  const onShare = async () => {
+    try {
+      const result = await Share.share({
+        message: "https://christmas4kids.org/volunteer/",
+        url: "https://christmas4kids.org/volunteer/",
+        title: "https://christmas4kids.org/volunteer/",
+      })
+    } catch (error) {
+      alert(error.message)
+    }
+  }
 
   return (
-    <View style={styles.socialMediaShareCard}>
-      <ImageBackground
-        source={require("../assets/images/gradient.png")}
-        style={{
-          overflow: "hidden",
-          borderRadius: 10,
-          flexDirection: "row",
-          flex: 1,
-          justifyContent: "center",
-        }}
-      >
-        <View style={styles.socialMediaShareCardLeftText}>
-          <Text style={styles.socialMediaShareCardLeftText}>
-            HELP US REACH MORE PEOPLE
-          </Text>
-        </View>
-        <View style={styles.socialMediaShareCardRightText}>
-          <View style={styles.socialMediaShareCardRightView}>
-            <Text
-              style={{
-                fontFamily: "ZillaSlab-Medium",
-                fontStyle: "normal",
-                fontWeight: "600",
-                fontSize: 8,
-                color: "#FFF",
-              }}
-            >
-              Share C4K On Your Socials:
-            </Text>
-            <View style={styles.socialMediaShareCardIcons}>
-              <FontAwesome5 name="facebook-square" size={14} color="#FFF" />
-              <FontAwesome5 name="instagram" size={14} color="#FFF" />
-              <FontAwesome5 name="twitter" size={14} color="#FFF" />
-              <FontAwesome5 name="linkedin-in" size={14} color="#FFF" />
+    <TouchableOpacity onPress={onShare}>
+      <View style={styles.socialMediaShareCard}>
+        <ImageBackground
+          source={require("../assets/images/gradient.png")}
+          style={{
+            overflow: "hidden",
+            borderRadius: 10,
+            flexDirection: "row",
+            flex: 1,
+            justifyContent: "center",
+          }}
+        >
+          <View style={styles.socialMediaShareCardLeft}>
+            <Text style={styles.socialMediaShareCardLeftText}>help us reach more people</Text>
+          </View>
+          <View style={styles.socialMediaShareCardRight}>
+            <View style={styles.socialMediaShareCardRightText}>
+              <Text style={styles.socialMediaShareCardRightText}>Share C4K On Your Socials:</Text>
+              <View style={styles.socialMediaShareCardIcons}>
+                <FontAwesome5 name="facebook-square" size={socialImageSize} color="#FFF" />
+                <FontAwesome5 name="instagram" size={socialImageSize} color="#FFF" />
+                <FontAwesome5 name="twitter" size={socialImageSize} color="#FFF" />
+                <FontAwesome5 name="linkedin-in" size={socialImageSize} color="#FFF" />
+              </View>
             </View>
           </View>
-        </View>
-      </ImageBackground>
-    </View>
+        </ImageBackground>
+      </View>
+    </TouchableOpacity>
   )
 }
 
