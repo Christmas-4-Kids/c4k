1d4d9fcfb59899a73d18f0c43c619e63209b4722
Thomas Harris <loden16harris@gmail.com>
2021-12-07 18:16:04 -0600

    fix: styling

--- screens/Schedule.tsx
+++ screens/Schedule.tsx
@@ -1,53 +1,51 @@
 import React, { useState, useEffect } from "react"
 import { SafeAreaView, ScrollView, View, Text, Button, TouchableOpacity } from "react-native"
+import { Card } from "../components/Card"
 import { useStyles } from "../context/styles.context"
 import firestore from "firebase/firestore"
 import { fetchSchedule } from "../services/firestore.service"
 import ScreenWrapper from "./ScreenWrapper"
- 
-export const Schedule = props => {
+import { ScheduleCard } from "../components/ScheduleCard"
+import { useUser } from "../context/user.context"
+import { Loading } from "./Loading"
+
+export const Schedule = (props) => {
   const { styles } = useStyles()
+  const { user } = useUser()
+  console.log("volunteerType", user.volunteerType)
+  const volunteerType = "ADMIN"
 
-  const [schedule, setSchedule] = useState(null)
+  const [schedule, setSchedule] = useState([])
 
   useEffect(() => {
-    fetchSchedule().then(data => {
+    fetchSchedule().then((data) => {
       setSchedule(data.data[0].events)
-      console.log("here is the schedule",data.data[0].events)
     })
-  }, [])
-
-  useEffect(() => {
-    let list = []
-    // const unsubscribe = firestore()
-    //   .collection("schedule")
-    //   .onSnapshot({
-    //     error: (e) => console.error(e),
-    //     next: (querySnapshot) => {
-    //       querySnapshot.forEach((doc) => {
-    //         let d = doc.data();
-    //         d.id = doc.id;
-    //         d.events.sort((event) => event.order);
-    //         list.push(d);
-    //       });
-    //       setSchedule(list[0]);
-    //     },
-    //   });
-    // return () => {
-    //   unsubscribe();
-    // };
   }, [])
   return (
     <ScreenWrapper>
+      <Card overrideStyles={styles.rulesHeaderCard}>
+        <Text style={styles.rulesTabHeader}>Shopping Day Schedule</Text>
+        <Text style={styles.rulesTabSubtext}>
+          Everything you need to know about being a chaperone on the big
+          shopping day.
+        </Text>
+      </Card>
+
       <View style={styles.sectionContainer}>
-        <ScrollView contentInsetAdjustmentBehavior="automatic" contentContainerStyle={{ flexGrow: 1 }}>
-          {!!schedule &&
-            schedule.map(event => (
-              <View style={styles.sectionContainer} key={event.order}>
-                <Text style={styles.sectionTitle}>{event.time}</Text>
-                <Text style={styles.sectionDescription}>{event.name}</Text>
-              </View>
-            ))}
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          contentContainerStyle={{ flexGrow: 1 }}
+        >
+          {schedule.length > 0 ? (
+            schedule.map((event) => (
+              <React.Fragment key={event.order}>
+                <ScheduleCard data={event} volunteerType={volunteerType} />
+              </React.Fragment>
+            ))
+          ) : (
+            <Loading />
+          )}
         </ScrollView>
       </View>
     </ScreenWrapper>
