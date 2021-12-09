5a9263f5a2d82e54c25f18715f55ff74112ea781
Conner Bush <cbush@evernote.com>
2021-12-09 09:58:30 -0600

    fixes to work with iOS

--- components/RulesGroup.tsx
+++ components/RulesGroup.tsx
@@ -1,19 +1,16 @@
 import React from "react"
 import { useState } from "react"
 import { useStyles } from "../context/styles.context"
-import { View, Text, CheckBox } from "react-native"
-import { AntDesign } from "@expo/vector-icons"
+import { View, Text, Pressable } from "react-native"
+import { AntDesign, MaterialCommunityIcons } from "@expo/vector-icons"
 
 const RulesGroup = ({ groupName, rules }) => {
   const [clicked, setClicked] = useState(false)
   const { styles } = useStyles()
-
+  console.log(`rules`, rules)
   return (
     <View style={styles.ruleCard}>
-      <View
-        onTouchStart={() => setClicked(!clicked)}
-        style={clicked ? styles.ruleCardTitle : styles.ruleCardTitleCollapsed}
-      >
+      <View onTouchStart={() => setClicked(!clicked)} style={clicked ? styles.ruleCardTitle : styles.ruleCardTitleCollapsed}>
         <View
           style={{
             alignItems: "center",
@@ -21,13 +18,7 @@ const RulesGroup = ({ groupName, rules }) => {
             flexDirection: "row",
           }}
         >
-          <Text>
-            {clicked ? (
-              <AntDesign name="minuscircleo" size={24} color="#fff" />
-            ) : (
-              <AntDesign name="pluscircleo" size={24} color="#FFF" />
-            )}
-          </Text>
+          <Text>{clicked ? <AntDesign name="minuscircleo" size={24} color="#fff" /> : <AntDesign name="pluscircleo" size={24} color="#FFF" />}</Text>
           <Text
             style={{
               color: "#FFF",
@@ -50,6 +41,7 @@ const RulesGroup = ({ groupName, rules }) => {
               fontFamily: "ZillaSlab-Medium",
               fontWeight: "600",
               fontSize: 12,
+              color: "#1B2C39",
             }}
           >
             {rules.map((rule, index) => (
@@ -68,19 +60,22 @@ const CheckListItem = ({ item }) => {
   const [isSelected, setSelection] = useState(false)
 
   return (
-    <View style={{ flexDirection: "row", paddingBottom: 20 }}>
-      <Text>
-        <CheckBox value={isSelected} onValueChange={setSelection} />
-      </Text>
-      <Text
-        style={{
-          flex: 1,
-          paddingLeft: 5,
-          color: isSelected ? "#CFCFCF" : "black",
-        }}
-      >
-        {item}
-      </Text>
-    </View>
+    <Pressable onPress={() => setSelection(!isSelected)}>
+      <View style={{ flexDirection: "row", paddingLeft: 10, paddingTop: 15 }}>
+        <Text>
+          {isSelected ? <MaterialCommunityIcons name="checkbox-marked" size={24} color="green" /> : <MaterialCommunityIcons name="checkbox-blank-outline" size={24} color="gray" />}
+        </Text>
+        <Text
+          style={{
+            paddingLeft: 5,
+            color: isSelected ? "#CFCFCF" : "#1B2C39",
+            fontFamily: "ZillaSlab-Medium",
+            fontSize: 14,
+          }}
+        >
+          {item}
+        </Text>
+      </View>
+    </Pressable>
   )
 }
--- screens/Rules.tsx
+++ screens/Rules.tsx
@@ -1,43 +1,29 @@
 import React, { useState, useEffect } from "react"
-import { Text, View } from "react-native"
+import { View } from "react-native"
 import { fetchRules } from "../services/firestore.service"
 import { Loading } from "./Loading"
-import { useStyles } from "../context/styles.context"
 import { useUser } from "../context/user.context"
-import ScreenWrapper from "./ScreenWrapper"
 import StickyScreenWrapper from "./StickyScreenWrapper"
-import { Card } from "../components/Card"
 import RulesGroup from "../components/RulesGroup"
 
 export const Rules = () => {
   const [rules, setRules] = useState([])
   const [groupedRules, setGroupedRules] = useState([])
 
-  const { styles } = useStyles()
   const { user } = useUser()
-  console.log("volunteerType", user.volunteerType)
-  // const volunteerType = user.volunteerType.slice(5) //removes 2021_ prefix
-  const volunteerType = "ADMIN"
+  const volunteerType = user.volunteerType.slice(5) //removes 2021_ prefix
 
   useEffect(() => {
-    fetchRules().then((data) => {
-      setRules(data.data)
+    fetchRules().then(data => {
+      setRules((data as any).data)
     })
   }, [])
 
   useEffect(() => {
-    const uniqueGroups = [
-      ...new Set(
-        rules
-          .filter((rule) => rule.volunteerTypes?.includes(volunteerType))
-          .map((rule) => rule.group)
-      ),
-    ]
+    const uniqueGroups = [...new Set(rules.filter(rule => rule.volunteerTypes?.includes(volunteerType)).map(rule => rule.group))]
     const gRules = []
     for (const group of uniqueGroups) {
-      const orderedGroupRulesByGroup = rules
-        .filter((rule) => rule.group === group)
-        .sort((first, last) => first.order - last.order)
+      const orderedGroupRulesByGroup = rules.filter(rule => rule.group === group).sort((first, last) => first.order - last.order)
       gRules.push({ group, rules: orderedGroupRulesByGroup })
     }
     // sorts the groups themselves
@@ -47,22 +33,18 @@ export const Rules = () => {
 
   return (
     <StickyScreenWrapper>
+      {rules.length > 0 ? groupedRules.map((item, index) => <RulesGroup key={index} rules={item.rules} groupName={item.group} />) : <Loading />}
       {rules.length > 0 ? (
-        groupedRules.map((item, index) => (
-          <RulesGroup key={index} rules={item.rules} groupName={item.group} />
-        ))
-      ) : (
-        <Loading />
-      )}
-      <RulesGroup
-        rules={[
-          {
-            description:
-              "The children and the Christmas4Kids Staff want to thank you again for spending your day with us - we hope you enjoy it as much as we do! If you need anything or have any questions do not hesitate to call.",
-          },
-        ]}
-        groupName={"Thank You!"}
-      />
+        <RulesGroup
+          rules={[
+            {
+              description:
+                "The children and the Christmas4Kids Staff want to thank you again for spending your day with us - we hope you enjoy it as much as we do! If you need anything or have any questions do not hesitate to call.",
+            },
+          ]}
+          groupName={"Thank You!"}
+        />
+      ) : null}
       {/* Added this blank View so that very last rule card displays. it wasn't scrolling down all the way for some reason */}
       <View style={{ height: 110 }}></View>
     </StickyScreenWrapper>
